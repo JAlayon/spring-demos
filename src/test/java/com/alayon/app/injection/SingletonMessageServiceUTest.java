52d07655357ff5bf8d25c862fa6c91aec4148a34
Jair Alayon <jair.atl.95@gmail.com>
2019-09-21 10:05:38 -0500

    adding simple test with junit

---
+++ src/test/java/com/alayon/app/injection/SingletonMessageServiceUTest.java
@@ -0,0 +1,25 @@
+package com.alayon.app.injection;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SingletonMessageServiceUTest {
+
+	private SingletonMessageService sms;
+	
+	@Before
+	public void initTests() {
+		sms = new SingletonMessageService();
+	}
+	
+	@Test
+	public void testMessage() {
+		for(int i = 1; i <= 9; i++) {
+			assertThat(String.valueOf(i), is(equalTo(sms.getMessage().substring(0,1))));
+		}
+	}
+	
+}
